55b777d845b97bbaeec9b6a3aeadb1025f244a0f
LRapalo <luisrapalo987@gmail.com>
2022-10-21 14:21:39 -0600

    INICIO

---
+++ src/lab/p2_luisrapalo_12141307/LabP2_LuisRapalo_12141307.java
@@ -0,0 +1,16 @@
+package lab.p2_luisrapalo_12141307;
+
+import java.util.Scanner;
+import java.util.Random;
+        
+public class LabP2_LuisRapalo_12141307 {
+
+static Scanner lea = new Scanner(System.in);
+static Random r = new Random ();
+
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
